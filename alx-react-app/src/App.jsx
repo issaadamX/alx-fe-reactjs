36fa1b1c20d567a2b81bb5d79427a96204a445b0
unknown <issaadamx@gmail.com>
2025-08-01 21:42:08 +0200

    Added welcome message

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/welcomeMessage'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
   )
 }
