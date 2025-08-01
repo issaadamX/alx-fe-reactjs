e4e2ed90dd04feaff47eea2825645d628215a419
unknown <issaadamx@gmail.com>
2025-08-01 23:26:31 +0200

    .

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,12 @@
 import UserContext from './UserContext';
-import ProfilePage from './components/ProfilePage';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", age: 30, bio: "Software developer" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
