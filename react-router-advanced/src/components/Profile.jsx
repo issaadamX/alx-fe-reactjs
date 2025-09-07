b0646ca72b114cab96e8567932ccd647fd46ca8e
issaadamX <issaadamx@gmail.com>
2025-09-08 00:37:40 +0200

    .

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,12 +1,18 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 const Profile = () => {
   return (
     <div>
       <h1>Profile Page</h1>
       <nav>
-        <Link to="/profile/details">Details</Link> | <Link to="/profile/settings">Settings</Link>
+        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
       <Outlet />
     </div>
   );
