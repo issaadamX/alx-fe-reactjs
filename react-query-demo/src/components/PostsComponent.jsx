22e1a44d2f40bb65641e52fd14848c92c7267cf4
issaadamX <issaadamx@gmail.com>
2025-09-07 23:59:54 +0200

    .

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,6 +12,10 @@ function PostsComponent() {
   const { data, isError, isLoading, error, refetch } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
+    cacheTime: 1000 * 60 * 5, // 5 minutes
+    staleTime: 1000 * 60, // 1 minute
+    refetchOnWindowFocus: true,
+    keepPreviousData: true,
   });
 
   if (isLoading) return <div>Loading...</div>;
