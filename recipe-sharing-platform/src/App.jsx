3fe11fab4b08432227b1d2fef2056cc40117324a
issaadamX <issaadamx@gmail.com>
2025-09-07 10:53:37 +0200

    .

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
-import AddRecipeForm from './components/AddRecipeForm';
+import AddRecipeForm from './components/AddRecipeForm.js';
 import './App.css';
 
 function App() {
