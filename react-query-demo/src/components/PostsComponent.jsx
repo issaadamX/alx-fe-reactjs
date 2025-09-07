96df3d53fdfb70342f33800e1b47b2b428bcfd11
issaadamX <issaadamx@gmail.com>
2025-09-07 23:49:03 +0200

    .

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,19 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
 function PostsComponent() {
-  const { data, error, isLoading, refetch } = useQuery({
+  const { data, isError, isLoading, refetch } = useQuery({
     queryKey: ['posts'],
-    queryFn: () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()),
+    queryFn: fetchPosts,
   });
 
   if (isLoading) return <div>Loading...</div>;
 
-  if (error) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error fetching posts.</div>;
 
   return (
     <div>
       <h1>Posts</h1>
-      <button onClick={refetch}>Refetch Posts</button>
+      <button onClick={() => refetch()}>Refetch Posts</button>
       <ul>
         {data.map(post => (
           <li key={post.id}>{post.title}</li>
